c3a3351e2c93e1778633123f2936bd8d85d91453
ivanbensa <ivanbensa@proton.me>
2026-05-10 16:58:52 +0200

    add lesson 2 variables practice

---
+++ 02-variables/README.md
@@ -0,0 +1,53 @@
+# JavaScript Lesson 2 - Practice Tasks
+
+## Task 1
+
+Create variables for:
+- your full name
+- your age
+- your favorite city
+- whether you can speak German
+
+## Task 2
+
+Create variables for:
+- ticket price
+- number of friends
+
+Then calculate:
+- total price
+- how much each person has to pay if 2 people pay together
+
+## Task 3
+
+Change the value of a variable.
+
+## Task 4
+
+Which variable names are valid and which are invalid?
+
+```js
+let favoriteBook = "Harry Potter";
+let city2 = "Berlin";
+
+let 2city = "Berlin";
+let full name = "John";
+```
+
+## Task 5
+
+What is the difference between:
+
+```js
+==
+===
+```
+Give one example
+
+## Bonus
+
+The cinema gives a 10% discount.
+
+Create:
+- discounted price
+- variable that checks if 40€ is enough to pay
---
+++ 02-variables/script.js
@@ -0,0 +1,55 @@
+//  Task 1
+
+let fullName = "Ivan Bensa";
+let age = 36;
+let favoriteCity = "Berlin";
+let speaksGerman = false;
+
+//  Task 2
+/*
+let ticketPrice = 12;
+let numberOfFriends = 5;
+
+let totalPrice = ticketPrice * numberOfFriends;
+let twoFriendsPay = totalPrice / 2;
+
+console.log(twoFriendsPay);
+*/
+//  Task 3
+
+let originalVariable = "Value of a variable"
+originalVariable = "Changed";
+
+console.log(originalVariable);
+
+//  Task 4
+
+//  let favoriteBook = "Harry Potter";  ->  valid
+//  let city2 = "Berlin";               ->  valid
+//
+//  let 2city = "Berlin";               ->  not
+//  let full name = "John";             ->  not
+
+//  Task 5
+
+// 10 == "10";  ->  compares values only
+// 10 === "10"; -> compares value and data
+
+//  Task 6 Bonus
+
+let ticketPrice = 12;
+let numberOfFriends = 5;
+let budget = 40;
+
+let totalPrice = ticketPrice * numberOfFriends;
+let twoFriendsPay = totalPrice / 2;
+
+console.log(twoFriendsPay);
+
+let discountedPrice = totalPrice * 0.9;
+let canPay = budget >= discountedPrice;
+
+console.log(canPay);
+
+
+
