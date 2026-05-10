4fdab1000aaf2344be48f32527c40b1f5f78f652
ivanbensa <ivanbensa@proton.me>
2026-05-10 14:03:49 +0200

    initial commit

---
+++ 01-js-basic/README.md
@@ -0,0 +1,45 @@
+# JavaScript Lesson 1 - Practice Tasks
+## Task 1
+
+What is the result?
+
+```js
+1 + 1
+"1" + "1"
+10 / 2
+5 ** 2
+```
+
+## Task 2
+
+Write:
+- one Number
+- one String
+- one Boolean
+
+## Task 3
+
+Fix the string:
+
+```js
+"He said: "Hello""
+```
+
+## Task 4
+
+What will these return?
+
+```js
+5 > 3
+10 === "10"
+10 !== "10"
+```
+
+## Task 5
+
+What is the difference between:
+
+```js
+==
+===
+```
---
+++ 01-js-basic/script.js
@@ -0,0 +1,30 @@
+//  Task 1
+
+//  1 + 1       ->  2
+// "1" + "1"    ->  "11"
+// 10 / 2       ->  5
+// 5 ** 2       ->  25
+
+//  Task 2
+
+//  Number      ->  1
+//  String      ->  "1"
+//  Boolean     ->  true
+
+// Task 3
+
+//  "He said: "Hello""  ->  'He said: "Hello"'
+
+//  Task 4
+
+//  5 > 3       ->  true
+// 10 === "10"  ->  false
+// 10 !== "10"  ->  true
+
+//  Task 5
+
+//  ==   -> compares values only
+//  ===  -> compares value AND data type
+
+//  10 == "10"   -> true
+//  10 === "10"  -> false
